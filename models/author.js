3503025c18f87497ffa823c51cf2ab98d766ee15
Canberk Varli <canberkvarli@gmail.com>
2025-02-03 19:56:25 -0800

    first commit

---
+++ models/author.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  last_name: { type: String, maxLength: 100 },
+  date_of_birth: Date,
+  date_of_death: Date,
+});
+
+AuthorSchema.pre("remove", () => {
+  Book.find({ author: this.id }, (err, books) => {
+    if (err) {
+      next(err);
+    } else if (books.length > 0) {
+      next(new Error("This author still has books"));
+    } else {
+      next();
+    }
+  });
+});
+
+AuthorSchema.virtual("name").get(function () {
+  if (this.first_name && this.last_name) {
+    return `${this.last_name}, ${this.first_name}`;
+  } else {
+    return "";
+  }
+});
+
+AuthorSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/catalog/author/${this._id}`;
+});
+
+module.exports = mongoose.model("Author", AuthorSchema);
