48e22793299d40aafe6b783127ea67f89c86c8a3
Canberk Varli <canberkvarli@gmail.com>
2025-02-05 11:49:33 -0800

    add form and validations for genre form

--- models/genre.js
+++ models/genre.js
@@ -3,7 +3,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const GenreSchema = new Schema({
-  name: { type: String, required: true, minLength: 3, maxLength: 100 },
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 100,
+    unique: true,
+  },
   books: [{ type: Schema.Types.ObjectId, ref: "Book" }],
 });
 
